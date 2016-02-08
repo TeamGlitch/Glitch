2647c8d9ef9952e003d28c9919b440c12d3146d3
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-02-08 15:41:11 +0100

    ErrorBox ability improved

---
+++ Assets/_Scripts/ErrorBoxScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ErrorBoxScript : MonoBehaviour {
+
+	public Vector3 positionWhenNotActive;
+
+	public bool boxActive;
+	public bool canBeActivated;
+	private float timeInitiated;
+	private float timeUnactive;
+
+	// Use this for initialization
+	void Start () {
+		timeInitiated = 0.0f;
+		timeUnactive = 8.0f;
+		boxActive = false;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		if (boxActive) {
+			timeInitiated += Time.deltaTime;
+			if (timeInitiated > 5.0f) {
+				boxActive = false;
+				transform.position = positionWhenNotActive;
+				timeUnactive = 0.0f;
+			}
+		} else if ((canBeActivated == false) && (timeUnactive >= 8.0f)) {
+			canBeActivated = true;
+		} else if (canBeActivated == false) {
+			timeUnactive += Time.deltaTime;
+		}
+	}
+
+	public void ActivateBox(Vector3 wantedPosition) {
+		boxActive = true;
+		canBeActivated = false;
+		timeInitiated = 0.0f;
+		transform.position = wantedPosition;
+	}
+}
