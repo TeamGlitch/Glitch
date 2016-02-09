5b9c6e81530febec0d276d59d48000666cd2939b
Raul Ramos Macias <raulrm2@TTC.CAT>
2016-02-09 19:31:01 +0100

    - Improved playercontroller and errorbox

--- Assets/_Scripts/ErrorBoxScript.cs
+++ Assets/_Scripts/ErrorBoxScript.cs
@@ -3,40 +3,16 @@ using System.Collections;
 
 public class ErrorBoxScript : MonoBehaviour {
 
-	public Vector3 positionWhenNotActive;
-
-	public bool boxActive;
-	public bool canBeActivated;
-	private float timeInitiated;
-	private float timeUnactive;
-
-	// Use this for initialization
-	void Start () {
-		timeInitiated = 0.0f;
-		timeUnactive = 8.0f;
-		boxActive = false;
-	}
+	public PlayerController player;
+	private float elapsedTime = 0;
+	public float duration = -1;
 		
 	// Update is called once per frame
 	void Update () {
-		if (boxActive) {
-			timeInitiated += Time.deltaTime;
-			if (timeInitiated > 5.0f) {
-				boxActive = false;
-				transform.position = positionWhenNotActive;
-				timeUnactive = 0.0f;
-			}
-		} else if ((canBeActivated == false) && (timeUnactive >= 8.0f)) {
-			canBeActivated = true;
-		} else if (canBeActivated == false) {
-			timeUnactive += Time.deltaTime;
+		elapsedTime++;
+		if (elapsedTime >= duration) {
+			player.GetComponent<PlayerController> ().errorBoxDeleted (1);
+			Destroy (gameObject);
 		}
-	}
-
-	public void ActivateBox(Vector3 wantedPosition) {
-		boxActive = true;
-		canBeActivated = false;
-		timeInitiated = 0.0f;
-		transform.position = wantedPosition;
 	}
 }
