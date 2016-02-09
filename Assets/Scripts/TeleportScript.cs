b84574097aa632ccf5ba768e665c65b9c3bac511
IzabranTebras <krystyan.fs@gmail.com>
2016-02-09 13:32:58 +0100

    Reorganization of project

--- Assets/Scripts/TeleportScript.cs
+++ Assets/Scripts/TeleportScript.cs
@@ -2,23 +2,14 @@
 using System.Collections;
 
 public class TeleportScript : PlayerController {
-    public bool coolDown;
 
     public void RightTeleport(int direction)
     {
-        if (coolDown == false)
+        Vector3 newPosition = new Vector3(transform.localPosition.x + (transform.localScale.x * 2) * direction, transform.localPosition.y + 0.1f, transform.localPosition.z);
+        Vector3 halfExtents = new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, transform.localScale.z / 2);
+        if (!Physics.CheckBox(newPosition, halfExtents))
         {
-            Vector3 newPosition = new Vector3(transform.localPosition.x + (transform.localScale.x * 2) * direction, transform.localPosition.y + 0.1f, transform.localPosition.z);
-            Vector3 halfExtents = new Vector3(transform.localScale.x / 2, transform.localScale.y / 2, transform.localScale.z / 2);
-            if (!Physics.CheckBox(newPosition, halfExtents))
-            {
-                transform.Translate((transform.localScale.x * 2) * direction, 0.0f, 0.0f);
-
-                if (state == player_state.JUMP)
-                {
-                    coolDown = true;
-                }
-            }
+            transform.Translate((transform.localScale.x * 2) * direction, 0.0f, 0.0f);
         }
     }
 }
