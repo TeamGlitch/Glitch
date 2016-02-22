8252acbdffae1f072d5b29974b8ec77bb5304808
IzabranTebras <cristian.fernandezsua@gmail.com>
2016-02-22 01:38:58 +0100

    Added script for the collect of items

---
+++ Assets/_Scripts/GUICollects.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUICollects : MonoBehaviour {
+    private int items;
+    private Text itemNumber;
+
+    void Start()
+    {
+        items = 0;
+        itemNumber = GetComponent<Text>();
+        itemNumber.text = items.ToString();
+    }
+
+	public void Increment()
+    {
+        ++items;
+        itemNumber.text = items.ToString();
+    }
+
+    public void Decrement(int itemsDecremented)
+    {
+        items -= itemsDecremented;
+        itemNumber.text = items.ToString();
+    }
+}
