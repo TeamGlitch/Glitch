00fe6730d9ad2cd5258032e34f47719c026732b1
IzabranTebras <cristian.fernandezsua@gmail.com>
2016-03-06 19:43:53 +0100

    Added player freeze in intro

--- Assets/_Scripts/DynamicCamera.cs
+++ Assets/_Scripts/DynamicCamera.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class DynamicCamera : MonoBehaviour {
     public Transform player;
     public Camera mainCamera;
+    public GameObject powers;
 
     private int speed = 10;
     private float delay = 5.0f;
+    private PlayerController movePlayer;
+
+    void Start()
+    {
+        movePlayer = player.GetComponent<PlayerController>();
+    }
 
     void Update () {
         if (transform.position.x <= (player.position.x + 7))
@@ -17,9 +24,7 @@ public class DynamicCamera : MonoBehaviour {
                 transform.Translate(0.0f, 0.0f, speed*Time.deltaTime);
                 if (transform.position.z >= -15)
                 {
-                    mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -15);
-                    mainCamera.gameObject.SetActive(true);
-                    gameObject.SetActive(false);
+                    beginGame();
                 }
             }
         }
@@ -28,4 +33,13 @@ public class DynamicCamera : MonoBehaviour {
             transform.Translate((-Time.deltaTime) * speed, 0.0f, 0.0f);
         }
     }
+
+    void beginGame()
+    {
+        mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -15);
+        mainCamera.gameObject.SetActive(true);
+        movePlayer.enabled = true;
+        powers.SetActive(true);
+        gameObject.SetActive(false);
+    } 
 }
