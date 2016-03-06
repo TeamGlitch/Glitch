6e4b0a3f85adccfda03925318ca7fcf5672dc2d7
IzabranTebras <cristian.fernandezsua@gmail.com>
2016-03-06 03:03:44 +0100

    Added dynamic camera

---
+++ Assets/_Scripts/DynamicCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicCamera : MonoBehaviour {
+    public Transform player;
+    public Camera mainCamera;
+
+    private int speed = 10;
+    private float delay = 5.0f;
+
+    void Update () {
+        if (transform.position.x <= (player.position.x + 7))
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0.0f)
+            {
+                transform.Translate(0.0f, 0.0f, speed*Time.deltaTime);
+                if (transform.position.z >= -15)
+                {
+                    mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -15);
+                    mainCamera.gameObject.SetActive(true);
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            transform.Translate((-Time.deltaTime) * speed, 0.0f, 0.0f);
+        }
+    }
+}
--- Assets/_Scripts/MainCamera.cs
+++ Assets/_Scripts/MainCamera.cs
@@ -11,7 +11,7 @@ public class MainCamera : MonoBehaviour {
 
     void Update()
     {
-        float posy = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + 3, ref ySpeed, ySmooth);
+        float posy = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + 6, ref ySpeed, ySmooth);
         float posx = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + 7, ref xSpeed, xSmooth * Time.deltaTime);
         transform.position = new Vector3 (posx, posy, transform.position.z);
     } 
