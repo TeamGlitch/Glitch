fe00e29c579301151b89f5adb925dceddcd40f8a
MetroGoldyMayer <amphiros@gmail.com>
2016-03-31 01:28:30 +0200

    Added glitch effect on waiting

--- Assets/_Scripts/GUI/DialogueScript.cs
+++ Assets/_Scripts/GUI/DialogueScript.cs
@@ -4,18 +4,12 @@ using System.Collections;
 
 public class DialogueScript : MonoBehaviour {
 
-	//Dialogue Box Writing
-	public Text dialogueBoxText;				//The dialogue box text reference
-	public float waitBetweenLetters = 0.05f;	//How much time there is between letter and letter printing
+	public enum dialogueBoxState
+	{
+		WRITTING,
+		WAITING
+	};
 
-	private bool printing = false;				//Currently printing in the dialogue box
-	private string messageToPrint;				//Message to print in the dialogue box
-	private int printLetter = 0;				//Index of the currently printing letter
-	private float nextLetterTime = 0f;			//Time when the next letter will be printed
-
-	private int currentRandomLetter = 0;
-	private float waitBetweenBuggedLetters = 0.02f;
-	private float nextBuggedLetterTime = 0f;
 	private string[] randomLetters = new string[] {
 		"•","|","£","Ø","ƒ","º","¿","®","¡","»","µ","±",
 		"¶","÷","ª","ø","†","‡","‘","—","™","œ","©","€",
@@ -24,58 +18,98 @@ public class DialogueScript : MonoBehaviour {
 		"★","☂","♞","☯","☭","☢","☎","❄"
 	};
 
+	//State
+	private dialogueBoxState state = dialogueBoxState.WRITTING;
+
+	//Dialogue Box
+	public Text dialogueBoxText;				//The dialogue box text reference
+
+	//Writting state
+	public float waitBetweenLetters = 0.05f;	//How much time there is between letter and letter printing
+
+	private string messageToPrint;				//Message to print in the dialogue box
+	private int printLetter = 0;				//Index of the currently printing letter
+	private float nextLetterTime = 0f;			//Time when the next letter will be printed
+
+	private int currentRandomLetter = 0;				//Current bugged letter in the writting effect
+	private float waitBetweenBuggedLetters = 0.02f;		//Wait between bugged letter changes
+	private float nextBuggedLetterTime = 0f;			//When the next bugged letter will appear
+
+	//Waiting state
+	private int corruptedPosition = -1;			//What index letter is corrupted
+	private char originalChar;					//What original character was in it
+	private float timeToSolve = 0;				//When the corruption will be solved
+
+
 	// Use this for initialization
 	void Start () {
 		messageToPrint = "Pack my box with five dozen liquor jugs. The five boxing wizards jump quickly. How vexingly quick daft zebras jump!";
-		printing = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (printing) {
+		switch (state) {
 
-			//If we can print the next letter
-			if (Time.time > nextLetterTime) {
+			case dialogueBoxState.WRITTING: //Currently writting in the dialogue box
 
-				//Go to the next letter
-				printLetter++;
+					//If we can print the next letter
+				if (Time.time > nextLetterTime) {
 
-				if (printLetter >= messageToPrint.Length) {
+					//Go to the next letter
+					printLetter++;
 
-					//If the end has been reached, put the message directly and restore variables
-					dialogueBoxText.text = messageToPrint;
-					printing = false;
-					printLetter = 0;
+					if (printLetter >= messageToPrint.Length) {
 
-				} else {
+						//If the end has been reached, put the message directly and restore variables
+						dialogueBoxText.text = messageToPrint;
+						state = dialogueBoxState.WAITING;
+						printLetter = 0;
 
-					//If not, we send the message to print with color tags that make it invisible from the print
-					//letter to the end. We do this rather than doing subscripts because they mess the paragraph aligment
-					dialogueBoxText.text = messageToPrint.Insert (printLetter, randomLetters[currentRandomLetter] + "<color=#00000000>") + "</color>";
+					} else {
 
-					//Finally we determine when the next letter will appear
-					nextLetterTime = Time.time + waitBetweenLetters;
+						//If not, we send the message to print with color tags that make it invisible from the print
+						//letter to the end. We do this rather than doing subscripts because they mess the paragraph aligment
+						dialogueBoxText.text = messageToPrint.Insert (printLetter, randomLetters [currentRandomLetter] + "<color=#00000000>") + "</color>";
 
-				}
+						//Finally we determine when the next letter will appear
+						nextLetterTime = Time.time + waitBetweenLetters;
 
-			} else {
-				
-				if (Time.time > nextBuggedLetterTime) {
-					
+					}
+
+				} else if (Time.time > nextBuggedLetterTime) {
+
 					//We change the currently showed sign and we determine the next sign change
 					currentRandomLetter = Random.Range (0, randomLetters.Length);
 					dialogueBoxText.text = messageToPrint.Insert (printLetter, randomLetters [currentRandomLetter] + "<color=#00000000>") + "</color>";
 					nextBuggedLetterTime = Time.time + waitBetweenBuggedLetters;
 				}
+					
+			break;
 
-			}
+			case dialogueBoxState.WAITING: //Message on dialogue box. Waiting for next instruction
+			
+				if (corruptedPosition == -1) {
+					if(Random.value > 0.9925f){
+						corruptedPosition = Random.Range(0, dialogueBoxText.text.Length);
+						originalChar = dialogueBoxText.text[corruptedPosition];
+						timeToSolve = Time.time + Random.Range(0.05f, 0.4f);
+
+						string newText = dialogueBoxText.text.Remove(corruptedPosition, 1);
+						newText = newText.Insert(corruptedPosition, randomLetters[Random.Range(0, randomLetters.Length)]);
+						dialogueBoxText.text = newText;
+					}
+					
+				} else if (Time.time > timeToSolve){
+					string newText = dialogueBoxText.text.Remove(corruptedPosition, 1);
+					newText = newText.Insert(corruptedPosition, originalChar.ToString());
+					dialogueBoxText.text = newText;
+					corruptedPosition = -1;
+				}
+
+			break;
 
 		}
-
-
-
-
 
 
 	}
