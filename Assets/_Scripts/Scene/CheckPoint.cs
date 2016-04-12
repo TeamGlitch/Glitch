f6bd6505f0beaa34c1b792d979e70c0a73fcefba
Raul-Ramos <amphiros@gmail.com>
2016-04-12 21:25:06 +0200

    Checkpoint particle effect base

--- Assets/_Scripts/Scene/CheckPoint.cs
+++ Assets/_Scripts/Scene/CheckPoint.cs
@@ -2,12 +2,19 @@
 
 public class CheckPoint : MonoBehaviour {
 
+	public ParticleSystem particles;
 	public bool active = false;
+	private bool growing = false;
+
+	void Start(){
+		particles = gameObject.transform.FindChild ("Particles").gameObject.GetComponent<ParticleSystem>();
+	}
 
 	void OnTriggerEnter(Collider coll){
 		if((coll.gameObject.name == "Player") && !active)
         {
 			setThisAsCheckPoint(coll.gameObject);
+			growing = true;
 		}
 	}
 
@@ -22,5 +29,17 @@ public class CheckPoint : MonoBehaviour {
 		renderer.material.color = new Color (1, 0, 0, renderer.material.color.a);
 
 		active = true;
+	}
+
+	void Update(){
+		if (growing == true) {
+
+			var so = new UnityEditor.SerializedObject(particles);
+
+			so.FindProperty("ShapeModule.radius").floatValue += 0.4f * Time.deltaTime;
+			//so.FindProperty("EmissionModule.rate").floatValue += 9.0f * Time.deltaTime;
+			//so.FindProperty ("startSpeed").floatValue += 0.5f * Time.deltaTime;
+			so.ApplyModifiedProperties();
+		}
 	}
 }
