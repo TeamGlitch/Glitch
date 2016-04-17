b680b120410d3cff3211f2c8e8b26b0819a03db9
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-04-17 11:49:35 +0200

    Artichokes and lava stones added

--- Assets/_Scripts/Scene/SpringPlantScript.cs
+++ Assets/_Scripts/Scene/SpringPlantScript.cs
@@ -1,16 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InControl;
 
 public class SpringPlantScript : MonoBehaviour {
 
 	public PlayerController playerController;
-	public float forceToPlayerWhenEnter = 50.0f;
+	public float forceToPlayerWhenEnter = 10.0f;
+	public float forceToPlayerWhenEnterAndJump = 25.0f;
+
+	private Animator _animator;
+
+	void Start()
+	{
+		_animator = transform.GetComponent<Animator> ();
+	}
+
+	public void EndAnimation()
+	{
+		_animator.SetBool ("PlayerInside", false);
+	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
 			playerController.vSpeed = forceToPlayerWhenEnter;
+			_animator.SetBool ("PlayerInside", true);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			if(InputManager.ActiveDevice.Action1.IsPressed)
+				playerController.vSpeed = forceToPlayerWhenEnterAndJump;
 			playerController.RestartTeleportCooldown ();
 		}
 	}
