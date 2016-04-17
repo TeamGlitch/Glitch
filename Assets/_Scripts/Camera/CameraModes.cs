60fa9c7cfe555d6e00792ea9e210776668a3fbf3
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-04-17 17:19:07 +0200

    New camera test + change in movement player in dialogues

---
+++ Assets/_Scripts/Camera/CameraModes.cs
@@ -0,0 +1,15 @@
+﻿public enum camera_mode
+{
+	FOLLOWING,
+	ON_RAILS
+};
+
+//Class that includes all the camera changes information
+[System.Serializable]
+public class CameraBehaviour {
+	
+	public camera_mode mode = camera_mode.FOLLOWING;
+	public float upPivot;
+	public float downPivot;
+
+}
