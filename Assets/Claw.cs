26428a8cadc37b1ffe44c9b7bd72fc0bb5bfa619
MetroGoldyMayer <amphiros@gmail.com>
2016-05-05 06:04:26 +0200

    Level base & claw

---
+++ Assets/Claw.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Claw : MonoBehaviour {
+
+	Animation animation;
+
+	void Awake(){
+		animation = gameObject.GetComponent<Animation>();
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player")
+			animation.Play();
+	}
+}
