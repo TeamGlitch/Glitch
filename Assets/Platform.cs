ebbd872d772855684ab2e4a8780fe58ea53a5779
MetroGoldyMayer <amphiros@gmail.com>
2016-05-10 17:44:15 +0200

    Temp

---
+++ Assets/Platform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			collision.gameObject.transform.parent.parent = transform;
+		}
+	}
+
+	void OnTriggerExit (Collider collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			collision.gameObject.transform.parent.parent = null;
+		}
+	}
+}
---
+++ Assets/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float speed;
+	private float angle = 0;
+	private float distance = 0;
+
+	// Use this for initialization
+	void Start () {
+		distance = transform.localPosition.x;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		angle += speed;
+		transform.localPosition = new Vector3 (Mathf.Cos (angle) * distance, Mathf.Sin (angle) * distance, transform.localPosition.z);
+
+		if (angle > 2 * Mathf.PI)
+			angle -= 2 * Mathf.PI;
+	}
+}
