f9cc0771eeafa327ef776179435285b5dcb3687a
Raul-Ramos <raulrm2@TTC.CAT>
2016-05-19 21:43:36 +0200

    Continuing

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -4,17 +4,30 @@ using System.Collections;
 public class Fire : MonoBehaviour {
 
 	private bool active = false;
+	private Collider collider;
+	private Renderer renderer;
+
 	private float timeToChange = 0;
 
 	public float frequency;
+
+	void Start(){
+		collider = GetComponent<Collider>();
+		renderer = GetComponent<Renderer>();
+	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > timeToChange) {
 			if (active) {
 				active = false;
+				renderer.material.color = new Color (1f, 0, 0, 0.5f);
+				collider.enabled = false;
 			} else {
 				active = true;
+				renderer.enabled = true;
+				collider.enabled = true;
+				renderer.material.color = new Color (1f, 0, 0, 1f);
 			}
 			timeToChange = Time.time + frequency;
 		}
---
+++ Assets/WindPipeIrregularity.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindPipeIrregularity : MonoBehaviour {
+
+	public float slowTime;
+	public float fastTime;
+	public float slowIntensity;
+	public float fastIntensity;
+
+	private bool slow;
+	private float change = 0;
+
+	private WindPipe windpipe;
+
+	// Use this for initialization
+	void Start () {
+		windpipe = GetComponent<WindPipe>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > change) {
+			if (slow) {
+				slow = false;
+				change = Time.time + fastTime;
+				windpipe.windSpeed = fastIntensity;
+			} else {
+				slow = true;
+				change = Time.time + slowTime;
+				windpipe.windSpeed = slowIntensity;
+			}
+
+		}
+	}
+}
