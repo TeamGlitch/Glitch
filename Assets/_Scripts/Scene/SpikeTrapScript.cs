d03631bf5ab880a87c4ef2aa7b3960c4bbac8719
MetroGoldyMayer <amphiros@gmail.com>
2016-06-02 00:25:23 +0200

    Lot of different things
    - Added fire and smoke to the fireplace
    - Created video introduction
    - If the player waits 1 minute without touching anything in the main
    menu, the video introduction will play again
    - Fixed spike trap's leaf movement
    - Fixed slow fps bug
    - Moved Songs to Music

--- Assets/_Scripts/Scene/SpikeTrapScript.cs
+++ Assets/_Scripts/Scene/SpikeTrapScript.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum spikeTrapState
+{
+	RESTING,
+	WAITING,
+	JUMPING,
+	ACTIVE,
+	HIDING
+};
+
 public class SpikeTrapScript : MonoBehaviour
 {
 	public World world;
+
+	private spikeTrapState state;
+
 	public float timeTrapWaitsInActivation = 1.0f;
 	public float timeTrapWaitsInDeactivation = 2.0f;
 	public float lerpTime = 1.0f;
@@ -18,10 +30,8 @@ public class SpikeTrapScript : MonoBehaviour
 	Vector3 endPosition;
 	float currentLerpTime;
 
-	private bool activated = false;
-	private bool deactivated = false;
-	private float timeWhenActivated;
-	private float timeWhenDeactivated;
+	private float timeToJump;
+	private float timeToDeactivate;
 
 	// Use this for initialization
 	void Start ()
@@ -29,88 +39,81 @@ public class SpikeTrapScript : MonoBehaviour
 		startPosition = transform.position;
 		endPosition = transform.position + moveDistance;
 		currentLerpTime = 0.0f;
+		leavesParticle.Stop();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if(world.doUpdate)
-		{
-			if (activated && (timeWhenActivated > timeTrapWaitsInActivation))
-			{
+		if (world.doUpdate) {
+
+			switch (state) {
+
+			case spikeTrapState.WAITING:
+
+				if (!leavesJumped && leavesParticle.isStopped && Time.time >= timeToJump - 1.0f) {
+					leavesParticle.Stop();
+					leavesParticle.Clear();
+					leavesParticle.Play();
+				}
+
+				if (Time.time >= timeToJump) {
+					state = spikeTrapState.JUMPING;
+					if (!leavesJumped) {
+						leaves.SetActive(false);
+						leavesJumped = true;
+					}
+				}
+
+				break;
+
+			case spikeTrapState.JUMPING:
+
 				currentLerpTime += world.lag;
 				float lerpPercentage = currentLerpTime / lerpTime;
-				if (lerpPercentage > 1.0f)
-				{
-					currentLerpTime = 1.0f;
-					activated = false;
+				if (lerpPercentage > 1.0f) {
+					timeToDeactivate = Time.time + timeTrapWaitsInDeactivation;	
+					state = spikeTrapState.ACTIVE;
 				}
-				Vector3 temporalPos = Vector3.Lerp (startPosition, endPosition, lerpPercentage);
-				transform.position = temporalPos;
-				if (currentLerpTime == 1.0f)
-				{
-					activated = false;
-					timeWhenDeactivated = 0.0f;
+
+				transform.position = Vector3.Lerp (startPosition, endPosition, lerpPercentage);
+
+
+				break;
+
+			case spikeTrapState.ACTIVE:
+
+				if (Time.time >= timeToDeactivate) {
 					currentLerpTime = 0.0f;
-					deactivated = true;
+					state = spikeTrapState.HIDING;
 				}
-			}
-			else if (activated)
-			{
-				timeWhenActivated += world.lag;
-				if (timeWhenActivated > 0.5f && !leavesJumped) {
-					leaves.SetActive(false);
-					leavesJumped = true;
-				}
-                if(timeWhenActivated > timeTrapWaitsInActivation)
-                {
-                    SoundManager.instance.PlaySingle(trapSound);
-                }
-			}
-			else if (deactivated && (timeWhenDeactivated > timeTrapWaitsInDeactivation))
-			{
+
+				break;
+
+			case spikeTrapState.HIDING:
+
 				currentLerpTime += world.lag;
-				float lerpPercentage = currentLerpTime / lerpTime;
-				if (lerpPercentage > 1.0f)
-				{
+				float lerpPercentageO = currentLerpTime / lerpTime;
+				if (lerpPercentageO > 1.0f) {
 					currentLerpTime = 1.0f;
-					deactivated = false;
+					state = spikeTrapState.RESTING;
 				}
-				Vector3 temporalPos = Vector3.Lerp (endPosition, startPosition, lerpPercentage);
-				transform.position = temporalPos;
-				if(currentLerpTime == 1.0f)
-					deactivated = false;
-			}
-			else if(deactivated)
-			{
-				timeWhenDeactivated += world.lag;
+
+				transform.position = Vector3.Lerp (endPosition, startPosition, lerpPercentageO);
+
+				break;
+
 			}
 		}
 	}
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.CompareTag ("Player"))
+		if (other.CompareTag ("Player") && state == spikeTrapState.RESTING)
 		{
-			timeWhenActivated = 0.0f;
+			timeToJump = Time.time + timeTrapWaitsInActivation;
 			currentLerpTime = 0.0f;
-			activated = true;
-			if (!leavesJumped) {
-				leavesParticle.Play();
-			}
-		}
-	}
-
-	void OnTriggerExit (Collider other)
-	{
-		if (other.CompareTag ("Player"))
-		{
-			if (!activated)
-			{
-				timeWhenDeactivated = 0.0f;
-				currentLerpTime = 0.0f;
-				deactivated = true;
-			}
+			state = spikeTrapState.WAITING;
 		}
 	}
 }
