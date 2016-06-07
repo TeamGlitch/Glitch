c76782733ec9d9795a7147435c0bd4decd52bc20
MetroGoldyMayer <amphiros@gmail.com>
2016-06-07 06:38:57 +0200

    Loader creation of new lines added

--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -6,16 +6,20 @@ public class Loader : MonoBehaviour {
 
     public static Loader instance = null;
 
+    private static AsyncOperation async;
+    private static bool loading = false;
+
     public Text text;
     public Text percent;
 
-    private static AsyncOperation async;
-    private static Transform loaderTransform;
-    private static bool loading = false;
-
     private float nextLine = 0;
+    private GameObject child;
 
-    private string[] phrases = { "Loading enemies hostility", "Ensuring ragequit situations", "Rendering stereotypical hacker binary patterns" };
+    private string[] phrases = { "Loading enemies hostility", "Ensuring ragequit situations", "Rendering stereotypical hacker binary patterns",
+                               "Sharpening enemies weapons", "Coordinating IA stupidity", "Leaking memory", "Compiling innecesary break commands",
+                               "Making noise for no reason", "Retrieving nostalgia", "Making bad design decisions", "Pretending actual bugs are intended bugs",
+                               "Loading loading screens", "Initializing recursive recursive recursive functions functions functions", 
+                               "Making up false loading operations", "Looking at GitHub to see who made a code mistake"};
 	
 	// Update is called once per frame
     void Awake()
@@ -24,7 +28,7 @@ public class Loader : MonoBehaviour {
         if (instance == null){
             //if not, set it to this.
             instance = this;
-            loaderTransform = transform;
+            child = transform.GetChild(0).gameObject;
         }
         //If instance already exists:
         else if (instance != this)
@@ -32,6 +36,8 @@ public class Loader : MonoBehaviour {
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+
+        text.text = "*** Commondore 64 Basic V2 **** \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 	}
 
     void Update(){
@@ -39,7 +45,7 @@ public class Loader : MonoBehaviour {
         if (loading){
             if (async.isDone){
                 loading = false;
-                loaderTransform.GetChild(0).gameObject.SetActive(false);
+                child.SetActive(false);
             }
             else {
                 int percnt = (int) ((100 * async.progress) + (10 * (async.progress / 0.9f)));
@@ -47,19 +53,26 @@ public class Loader : MonoBehaviour {
 
                 if (Time.time > nextLine)
                 {
-                    text.text += "\n" + phrases[Random.Range(0, phrases.Length)];
-                    nextLine = Time.time + Random.Range(3.0f * 0, 9.0f * 0);
+                    text.text += "\n" + phrases[Random.Range(0, phrases.Length)] + ".";
+                    nextLine = Time.time + Random.Range(0.5f, 2.0f);
                 }
             }
         }
 
     }
 
-    public static void LoadScene(string levelName){
+    public static void LoadScene(string levelName)
+    {
         if (!loading){
             loading = true;
             async = Application.LoadLevelAsync(levelName);
-            loaderTransform.GetChild(0).gameObject.SetActive(true);
+            instance.prepareLoading();
         }
+    }
+
+    public void prepareLoading()
+    {
+        child.SetActive(true);
+        percent.text = "Now Loading: 0%";
     }
 }
