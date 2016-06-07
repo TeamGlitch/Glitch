8c160a9ab444c24214368f62d5563b74e0650a7a
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-06-07 14:55:53 +0200

    Lava texture moving

---
+++ Assets/MoveTexture.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTexture : MonoBehaviour {
+    public float scrollSpeed = 0.1f;
+    public Renderer rend;
+    public World world;
+    void Start() {
+        rend = GetComponent<Renderer>();
+    }
+    void Update() {
+        if(world.doUpdate)
+        {
+            float offset = world.lag * scrollSpeed;
+            rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        }
+    }
+}
