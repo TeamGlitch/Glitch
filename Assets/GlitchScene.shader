6bc1f3626722ec098d14658addb153f7f6a0160c
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-06-12 16:00:20 +0200

    Glitch advancing with the glitch

--- Assets/GlitchScene.shader
+++ Assets/GlitchScene.shader
@@ -3,6 +3,8 @@
 	Properties
 	{
 		_MainTex ("Texture", 2D) = "white" {}
+		_PercentagePixel ("PercentagePixel", float) = 0.2
+
 	}
 	SubShader
 	{
@@ -16,6 +18,8 @@
 			#pragma fragment frag
 			
 			#include "UnityCG.cginc"
+
+			uniform float _PercentagePixel;
 
 			struct appdata
 			{
@@ -43,13 +47,43 @@
 			{
 				float2 newUV;
 				half4 final;
-				if(i.uv.x > 0.2 && i.uv.x < 0.4 && i.uv.y < 0.3 && i.uv.y > 0.1)
-					final = tex2D(_MainTex, i.uv.xy);
-				else
+				if((i.uv.x < _PercentagePixel + 0.3 && (i.uv.y < 0.05 || i.uv.y > 0.95)) ||
+					(i.uv.x < _PercentagePixel + 0.45 && (i.uv.y < 0.025 || i.uv.y > 0.975)) ||
+					(i.uv.x < _PercentagePixel + 0.225 && (i.uv.y < 0.075 || i.uv.y > 0.925)) ||
+					(i.uv.x < _PercentagePixel + 0.150 && (i.uv.y < 0.1 || i.uv.y > 0.9)) ||
+					(i.uv.x < _PercentagePixel + 0.1125 && (i.uv.y < 0.125 || i.uv.y > 0.875)) ||
+					(i.uv.x < _PercentagePixel + 0.075 && (i.uv.y < 0.15 || i.uv.y > 0.85)) ||
+					(i.uv.x < _PercentagePixel + 0.05 && (i.uv.y < 0.175 || i.uv.y > 0.825)) ||
+					(i.uv.x < _PercentagePixel + 0.025 && (i.uv.y < 0.2 || i.uv.y > 0.8)) ||
+					(i.uv.x < _PercentagePixel + 0.012 && (i.uv.y < 0.225 || i.uv.y > 0.775)) ||
+					(i.uv.x < _PercentagePixel))
 				{
 					newUV.x = i.uv.x + (0.005 - (i.uv.x % 0.005));
 					newUV.y = i.uv.y + (0.005 - (i.uv.y % 0.005));
-					final = tex2D(_MainTex,  newUV);
+				    final = tex2D(_MainTex,  newUV);
+
+/*					if((_Time[3] % 5 > 0.5 &&  _Time[3] % 5 < 0.75) || _Time[3] % 5 > 1)
+					{
+						final -= (half4(0.1,0.1,0.1,0.0));
+					}
+					else
+					{
+						final -= (half4(0.15,0.15,0.15,0.0));
+					}*/
+
+/*					if((_Time[3] % 5 > 0.5 &&  _Time[3] % 5 < 0.75) || _Time[3] % 5 > 1)
+					{
+						final += (half4(0.488,0.125,0.57,0.0));
+					}
+					else
+					{
+						final += (half4(0.244,0.0625,0.285,0.0));
+					}
+					*/
+				}
+				else
+				{
+					final = tex2D(_MainTex,  i.uv.xy);
 				}
 
 				return final;
--- Assets/GlitchSceneShader.cs
+++ Assets/GlitchSceneShader.cs
@@ -5,37 +5,49 @@ public class GlitchSceneShader : ImageEffectBase
 {
     Texture2D texture;
     public int divisions = 100;
+    public Transform _playerTransform;
+    private float _maxXPos;
+    public float _percentagePixel = 0.0f;
+
+    private float correctionMarkerWidth = (-13.59751f * 11.02f);
+    private float correctionMarkerHeight = (-13.59751f * 14.9f);
+    private float correctionDueToAspect = 1.777605f;
+
+    public Canvas gui;
+    private RectTransform guiRectTrans;
 
     // Use this for initialization
     void Start()
     {
+        _maxXPos = _playerTransform.transform.position.x;
+        guiRectTrans = gui.GetComponent<RectTransform>();
     }
 
     // Called by camera to apply image effect
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-
+        _maxXPos = Mathf.Max(_maxXPos, _playerTransform.transform.position.x);
+        CalculatebPlayerPositionsInPercentage();
         //If it does, creates a 2D texture with 1x'divisions'
         //texel size and arbitrary asigns 0 and 2 to glitchy 
         //divisions and 1 to non-glitchy divisions
-        texture = new Texture2D(divisions, 1);
-
-        for (int z = 0; z < divisions; z += 1)
-        {
-            if (z < 20)
-            {
-                texture.SetPixel(z, 0, new Color32(128, 128, 128, 128));
-            }
-            else
-            {
-                texture.SetPixel(z, 0, new Color32(1, 0, 0, 0));
-            }
-        }
+        texture = new Texture2D(1, 1);
         texture.filterMode = FilterMode.Point;
         texture.Apply();
 
         material.SetTexture("_DispTex", texture);
+        material.SetFloat("_PercentagePixel", _percentagePixel);
 
         Graphics.Blit(source, destination, material);
+    }
+
+    private void CalculatebPlayerPositionsInPercentage()
+    {
+        Vector3 posToCalculate = _playerTransform.position;
+        posToCalculate.x = _maxXPos;
+        Vector3 camPosition = Camera.main.WorldToScreenPoint(posToCalculate);
+        camPosition.x *= guiRectTrans.rect.width / Camera.main.pixelWidth;
+        camPosition.y *= guiRectTrans.rect.height / Camera.main.pixelHeight;
+        _percentagePixel = camPosition.x / guiRectTrans.rect.width - 0.15f;
     }
 }
