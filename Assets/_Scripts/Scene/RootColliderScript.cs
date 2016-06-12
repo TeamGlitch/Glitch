e894ee951cb69ba6f1777df8975b4a8499d50857
IzabranTebras <krystyan.fs@gmail.com>
2016-06-12 19:56:02 +0200

    added functionality to root trap

---
+++ Assets/_Scripts/Scene/RootColliderScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RootColliderScript : MonoBehaviour {
+
+    public RootScript root;
+	
+	void OnTriggerStay(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            root.enabled = true;
+        }
+    }
+}
---
+++ Assets/_Scripts/Scene/RootScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RootScript : MonoBehaviour {
+
+    public World world;
+    public bool down = true;
+
+    private bool top;
+
+    void Start()
+    {
+        if (down)
+        {
+            top = true;
+        }
+        else
+        {
+            top = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (!coll.collider.CompareTag("Player"))
+        {
+            if (!down)
+            {
+                down = true;
+                if (top)
+                {
+                    enabled = false;
+                }
+            }
+            else
+            {
+                down = false;
+                if (!top)
+                {
+                    enabled = false;
+                }
+            }
+        }
+    }
+
+	void Update () {
+        if (down)
+        {
+            transform.Translate(0.0f, -world.lag * 20, 0.0f);
+        }
+        else
+        {
+            transform.Translate(0.0f, world.lag * 10, 0.0f);
+        }
+	}
+}
