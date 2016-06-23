c542b1999cabc10313d7cf94f193eea55de4ea76
MetroGoldyMayer <amphiros@gmail.com>
2016-06-23 05:21:18 +0200

    Options in-game
    - Options moved to its own script and prefab because it's reused
    - Options now on pause menu during game
    - Fixed things on menu

--- Assets/_Scripts/GUI/MenuScript.cs
+++ Assets/_Scripts/GUI/MenuScript.cs
@@ -15,31 +15,8 @@ public class MenuScript : MonoBehaviour {
     public Button levelSelectText;
     public Button firstLevelSelectButton;
 
-    public Canvas optionsMenu;
+    private OptionsMenuScript options;
     public Button OptionsText;
-    public Button ControlsButton;
-
-    public Canvas helpMenu;
-    public Button firstHelpButton;
-    public Button keyboardButton;
-    public Button gamepadButton;
-    public Button backButtonInHelp;
-    public Image keyboardImage;
-    public Image gamepadImage;
-
-    public Canvas graphicsMenu;
-    public Dropdown qualityLevel;
-    public Dropdown resolutions;
-    public Toggle fullscreen;
-
-    public Canvas audioMenu;
-    public Slider musicSlider;
-    public Slider soundSlider;
-    public Slider panSlider;
-    public Dropdown speakersMode;
-
-    public Canvas creditsMenu;
-    public Button firstCreditsButton;
 
 	public Canvas quitMenu;
     public Button exitText;
@@ -56,18 +33,16 @@ public class MenuScript : MonoBehaviour {
 
 	void Start () 
     {
-        optionsMenu.enabled = false;
+
 		quitMenu.enabled = false;
 		levelSelectionMenu.enabled = false;
-		helpMenu.enabled = false;
-        graphicsMenu.enabled = false;
-        audioMenu.enabled = false;
-		creditsMenu.enabled = false;
 		startText.Select ();
 
 		//Play the menu music and check this time as the last active
 		lastTimeActive = Time.time;
 		SoundManager.instance.musicSource.Play();
+
+        options =  GetComponent<OptionsMenuScript>();
 	}
 
 	void Update(){
@@ -123,8 +98,8 @@ public class MenuScript : MonoBehaviour {
     }
 
     public void OptionsPress(){
+
         SoundManager.instance.PlaySingle(confirmSound);
-        optionsMenu.enabled = true;
 
 		startText.enabled = false;
 		levelSelectText.enabled = false;
@@ -133,189 +108,7 @@ public class MenuScript : MonoBehaviour {
 
 		onMainScreen = false;
 
-		ControlsButton.Select ();
-    }
-
-    public void ReturnToOptions()
-    {
-        SoundManager.instance.PlaySingle(backSound);
-
-        optionsMenu.enabled = true;
-        helpMenu.enabled = false;
-        graphicsMenu.enabled = false;
-        audioMenu.enabled = false;
-        creditsMenu.enabled = false;
-
-        ControlsButton.Select();
-    }
-
-	public void HelpPress()
-	{
-        SoundManager.instance.PlaySingle(confirmSound);
-        helpMenu.enabled = true;
-        optionsMenu.enabled = false;
-
-		keyboardButton.enabled = false;
-		gamepadButton.enabled = true;
-
-		keyboardImage.enabled = true;
-		gamepadImage.enabled = false;
-
-		Navigation customNav = new Navigation();
-		customNav.mode = Navigation.Mode.Explicit;
-		customNav.selectOnUp = gamepadButton;
-		backButtonInHelp.navigation = customNav;
-
-		firstHelpButton.Select ();
-	}
-
-    public void GraphicsPress()
-    {
-
-        SoundManager.instance.PlaySingle(confirmSound);
-        graphicsMenu.enabled = true;
-        optionsMenu.enabled = false;
-
-        qualityLevel.options.Clear();
-        string[] names = QualitySettings.names;
-        for (int i = 0; i < names.Length; i++)
-        {
-            Dropdown.OptionData entry = new Dropdown.OptionData();
-            entry.text = names[i];
-            qualityLevel.options.Add(entry);
-        }
-        qualityLevel.value = QualitySettings.GetQualityLevel();
-
-        resolutions.options.Clear();
-        Resolution[] resolutionList = Screen.resolutions;
-        for(int i = 0; i < resolutionList.Length; i++)
-        {
-            Dropdown.OptionData entry = new Dropdown.OptionData();
-            entry.text = resolutionList[i].width + "x" + resolutionList[i].height;
-            resolutions.options.Add(entry);
-
-            if (resolutionList[i].width == Screen.width &&
-                resolutionList[i].height == Screen.height)
-            {
-                resolutions.value = i;
-            }
-        }
-
-        if (Screen.fullScreen)
-            fullscreen.isOn = true;
-        else
-            fullscreen.isOn = false;
-
-        resolutions.Select();
-
-    }
-
-
-    public void ChangeResolutionFullscreen()
-    {
-        Resolution newRes = Screen.resolutions[resolutions.value];
-        Screen.SetResolution(newRes.width, newRes.height, fullscreen.isOn);
-    }
-
-    public void ChangeQuality()
-    {
-        QualitySettings.SetQualityLevel(qualityLevel.value);
-    }
-
-    public void AudioPress() {
-        SoundManager.instance.PlaySingle(confirmSound);
-        audioMenu.enabled = true;
-        optionsMenu.enabled = false;
-
-        musicSlider.value = SoundManager.instance.musicSource.volume;
-        soundSlider.value = SoundManager.instance.efxSource1.volume;
-        panSlider.value = SoundManager.instance.musicSource.panStereo;
-
-        if (AudioSettings.speakerMode == AudioSpeakerMode.Mono)
-            speakersMode.value = 0;
-        else if(AudioSettings.speakerMode == AudioSpeakerMode.Stereo)
-            speakersMode.value = 1;
-        else if(AudioSettings.speakerMode == AudioSpeakerMode.Quad)
-            speakersMode.value = 2;
-        else if(AudioSettings.speakerMode == AudioSpeakerMode.Surround)
-            speakersMode.value = 3;
-        else if(AudioSettings.speakerMode == AudioSpeakerMode.Mode5point1)
-            speakersMode.value = 4;
-        else if(AudioSettings.speakerMode == AudioSpeakerMode.Mode7point1)
-            speakersMode.value = 5;
-        else if(AudioSettings.speakerMode == AudioSpeakerMode.Prologic)
-            speakersMode.value = 6;
-
-        musicSlider.Select();
-    }
-
-    public void ChangeMusicVolume(){
-        SoundManager.instance.musicSource.volume = musicSlider.value;
-    }
-
-    public void ChangeSoundVolume()
-    {
-        SoundManager.instance.efxSource1.volume = soundSlider.value;
-        SoundManager.instance.efxSource2.volume = soundSlider.value;
-        SoundManager.instance.efxSource3.volume = soundSlider.value;
-        SoundManager.instance.efxSource4.volume = soundSlider.value;
-        SoundManager.instance.efxSource5.volume = soundSlider.value;
-
-        SoundManager.instance.PlaySingle(confirmSound);
-    }
-
-    public void ChangePan(){
-
-        SoundManager.instance.musicSource.panStereo = panSlider.value;
-
-        SoundManager.instance.efxSource1.panStereo = panSlider.value;
-        SoundManager.instance.efxSource2.panStereo = panSlider.value;
-        SoundManager.instance.efxSource3.panStereo = panSlider.value;
-        SoundManager.instance.efxSource4.panStereo = panSlider.value;
-        SoundManager.instance.efxSource5.panStereo = panSlider.value;
-
-    }
-
-    public void ChangeSpeakerMode(){
-
-        float musicTime = -1;
-
-        if (SoundManager.instance.musicSource.isPlaying)
-        {
-            musicTime = SoundManager.instance.musicSource.time;
-        }
-
-        AudioConfiguration config = AudioSettings.GetConfiguration();
-
-        switch (speakersMode.value){
-            
-            case 0: config.speakerMode = AudioSpeakerMode.Mono; break;
-            case 1: config.speakerMode = AudioSpeakerMode.Stereo; break;
-            case 2: config.speakerMode = AudioSpeakerMode.Quad; break;
-            case 3: config.speakerMode = AudioSpeakerMode.Surround; break;
-            case 4: config.speakerMode = AudioSpeakerMode.Mode5point1; break;
-            case 5: config.speakerMode = AudioSpeakerMode.Mode7point1; break;
-            case 6: config.speakerMode = AudioSpeakerMode.Prologic; break;
-
-        }
-
-        AudioSettings.Reset(config);
-
-        if (musicTime != -1)
-        {
-            SoundManager.instance.musicSource.Play();
-            SoundManager.instance.musicSource.time = musicTime;
-        }
-
-    }
-
-    public void CreditsMenu()
-    {
-        SoundManager.instance.PlaySingle(confirmSound);
-        creditsMenu.enabled = true;
-        optionsMenu.enabled = false;
-
-        firstCreditsButton.Select();
+        options.Enable();
     }
 
 	public void QuitPress()
@@ -333,45 +126,12 @@ public class MenuScript : MonoBehaviour {
 		firstExitButton.Select ();
 	}
 
-	public void KeyboardPress()
-	{
-        SoundManager.instance.PlaySingle(confirmSound);
-        keyboardButton.enabled = false;
-		gamepadButton.enabled = true;
-		keyboardImage.enabled = true;
-		gamepadImage.enabled = false;
-
-		Navigation customNav = new Navigation();
-		customNav.mode = Navigation.Mode.Explicit;
-		customNav.selectOnUp = gamepadButton;
-		backButtonInHelp.navigation = customNav;
-
-		gamepadButton.Select ();
-	}
-
-	public void GamepadPress()
-	{
-        SoundManager.instance.PlaySingle(confirmSound);
-        keyboardButton.enabled = true;
-		gamepadButton.enabled = false;
-		keyboardImage.enabled = false;
-		gamepadImage.enabled = true;
-
-		Navigation customNav = new Navigation();
-		customNav.mode = Navigation.Mode.Explicit;
-		customNav.selectOnUp = keyboardButton;
-		backButtonInHelp.navigation = customNav;
-
-		keyboardButton.Select ();
-	}
-
 	public void NoPress()
 	{
         SoundManager.instance.PlaySingle(backSound);
         quitMenu.enabled = false;
 		levelSelectionMenu.enabled = false;
-		optionsMenu.enabled = false;
-		creditsMenu.enabled = false;
+        options.Disable();
 
 		startText.enabled = true;
 		levelSelectText.enabled = true;
