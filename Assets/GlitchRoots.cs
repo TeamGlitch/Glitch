f74377d657d9ced4f06bacebf9c7c1ccdbd3d164
IzabranTebras <krystyan.fs@gmail.com>
2016-06-23 19:54:17 +0200

    First version roots glitch

---
+++ Assets/GlitchRoots.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class GlitchRoots : MonoBehaviour {
+    public GameObject rootAssociated;
+    public Shader newShader;
+
+    private Shader rootShader;
+    private Renderer rootRender;
+
+    void Start()
+    {
+        rootShader = rootAssociated.GetComponent<Shader>();
+        rootRender = rootAssociated.GetComponent<Renderer>();
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (InputManager.ActiveDevice.Action4.WasPressed && coll.CompareTag("Player"))
+        {
+            rootRender.material.shader = newShader;
+        }
+    }
+}
