1758521bd28e2fe605caca54b2a25a9b6853e325
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-06-24 18:27:15 +0200

    Cameras, dead colliders, top side and shaders

--- Assets/TestShaders/GlitchSceneShader.cs
+++ Assets/TestShaders/GlitchSceneShader.cs
@@ -4,30 +4,42 @@ using UnityStandardAssets.ImageEffects;
 public class GlitchSceneShader : ImageEffectBase
 {
     Texture2D texture;
-    public Transform _playerTransform;
-	private Player _playerScript;
-    private float _maxXPos;
-
-	public float pixelSize = 0.005f;
+    public Transform playerTransform;
+    private Player playerScript;
+    private float maxXPos;
+    private GlitchLineShader glitchLineShader;
+    public float pixelSize = 0.005f;
 
     public Canvas gui;
     private RectTransform guiRectTrans;
 
+    public float initialXPos = -375f;
+
     // Use this for initialization
     void Start()
     {
-        _maxXPos = _playerTransform.transform.position.x;
-		_playerScript = _playerTransform.GetComponent<Player>();
+        maxXPos = initialXPos;
+        playerScript = playerTransform.GetComponent<Player>();
         guiRectTrans = gui.GetComponent<RectTransform>();
+        glitchLineShader = transform.GetComponent<GlitchLineShader>();
     }
 
     // Called by camera to apply image effect
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-		_maxXPos = _playerScript.lastCheckPoint.transform.position.x;
-		Vector3 maxPos = new Vector3(_maxXPos, _playerTransform.position.y, _playerTransform.position.z);
+        if (!playerScript.lastCheckPoint.startPoint)
+            maxXPos = playerScript.lastCheckPoint.transform.position.x;
+        if (maxXPos > playerScript.transform.position.x && !glitchLineShader.active &&
+            ((Time.unscaledTime % 20f >= 0f && Time.unscaledTime % 20f <= 3f) || (Time.unscaledTime % 20f >= 8f && Time.unscaledTime % 20f <= 12f)))
+            glitchLineShader.active = true;
+        else if (maxXPos > playerScript.transform.position.x && glitchLineShader.active &&
+            ((Time.unscaledTime % 20f > 3f && Time.unscaledTime % 20f < 8f) || (Time.unscaledTime % 20f > 12f && Time.unscaledTime % 20f < 20f)))
+            glitchLineShader.active = false;
+        else if (maxXPos < playerScript.transform.position.x && glitchLineShader.active)
+            glitchLineShader.active = false;
+        Vector3 maxPos = new Vector3(maxXPos, playerTransform.position.y, playerTransform.position.z);
         float percentageMaxPosition = CalculatebGlitchPositionsInPercentage(maxPos);
-		
+
         //If it does, creates a 2D texture with 1x'divisions'
         //texel size and arbitrary asigns 0 and 2 to glitchy 
         //divisions and 1 to non-glitchy divisions
@@ -37,17 +49,17 @@ public class GlitchSceneShader : ImageEffectBase
 
         material.SetTexture("_DispTex", texture);
         material.SetFloat("_PercentagePixel", percentageMaxPosition);
-		material.SetFloat("_PixelSize", pixelSize);
+        material.SetFloat("_PixelSize", pixelSize);
 
         Graphics.Blit(source, destination, material);
     }
 
-	private float CalculatebGlitchPositionsInPercentage(Vector3 posToCalculate)
+    private float CalculatebGlitchPositionsInPercentage(Vector3 posToCalculate)
     {
         Vector3 camPosition = Camera.main.WorldToScreenPoint(posToCalculate);
-		float xPercentagePositionInCamera;       
-		camPosition.x *= guiRectTrans.rect.width / Camera.main.pixelWidth;
+        float xPercentagePositionInCamera;
+        camPosition.x *= guiRectTrans.rect.width / Camera.main.pixelWidth;
         xPercentagePositionInCamera = camPosition.x / guiRectTrans.rect.width;
-		return xPercentagePositionInCamera;
+        return xPercentagePositionInCamera;
     }
 }
--- Assets/_Scripts/Scene/StartPoint.cs
+++ Assets/_Scripts/Scene/StartPoint.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StartPoint : CheckPoint {
+public class StartPoint : CheckPoint
+{
 
-	void Start () {
-		GameObject player = GameObject.Find("Player");
-		player.transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-		setThisAsCheckPoint(player, true);
-	}
+    void Start()
+    {
+        startPoint = true;
+        GameObject player = GameObject.Find("Player");
+        player.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        setThisAsCheckPoint(player, true);
+    }
 }
