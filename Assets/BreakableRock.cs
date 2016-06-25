1924edecc0f29cf94fd8d8a1111bdc0ec447f811
IzabranTebras <krystyan.fs@gmail.com>
2016-06-25 18:48:57 +0200

    Falling rocks complete with exclamation

--- Assets/BreakableRock.cs
+++ Assets/BreakableRock.cs
@@ -5,39 +5,47 @@ public class BreakableRock : MonoBehaviour {
 
     public Rigidbody[] rigidBodies;
     public Transform[] transforms;
-    public Rigidbody parent;
     public BoxCollider box;
 
+    private Vector3 initPosition;
+    private Quaternion initRot;
     private Vector3[] initialTrans;
+    private Quaternion[] initialRot;
     private BoxCollider[] boxes;
 
     void Start()
     {
+        initPosition = transform.position;
+        initRot = transform.rotation;
         initialTrans = new Vector3[transforms.Length];
+        initialRot = new Quaternion[transforms.Length];
         boxes = new BoxCollider[transforms.Length];
 
         for (int i = 0; i < transforms.Length; i++)
         {
-            initialTrans[i] = transforms[i].position;
+            initialTrans[i] = transforms[i].localPosition;
+            initialRot[i] = transforms[i].localRotation;
             boxes[i] = transform.GetChild(i).GetComponent<BoxCollider>();
         }
     }
 
-    public void RestartPos()
+    public void Restart()
     {
         for (int i = 0; i < transforms.Length; i++)
         {
             rigidBodies[i].isKinematic = true;
-            parent.isKinematic = true;
-            transforms[i].position = initialTrans[i];
+            transforms[i].localPosition = initialTrans[i];
+            transforms[i].localRotation = initialRot[i];
             boxes[i].enabled = false;
         }
+        transform.position = initPosition;
+        transform.rotation = initRot;
         box.enabled = true;
     }
 
     void OnCollisionEnter(Collision coll)
     {
-        if (coll.other.CompareTag("Player") || coll.other.CompareTag("Floor"))
+        if (coll.collider.CompareTag("Player") || coll.collider.CompareTag("Floor"))
         {
             box.enabled = false;
             for (int i = 0; i < rigidBodies.Length; i++)
@@ -45,7 +53,6 @@ public class BreakableRock : MonoBehaviour {
                 rigidBodies[i].isKinematic = false;
                 boxes[i].enabled = true;
             }
-            Invoke("RestartPos", 5.0f);
         }
     }
 }
--- Assets/GlitchRoots.cs
+++ Assets/GlitchRoots.cs
@@ -8,7 +8,9 @@ public class GlitchRoots : MonoBehaviour {
     public Shader plusShader;
     public RootsManager manager;
     public GameObject debris;
+    public Player player;
 
+    private BreakableRock[] debrisScript;
     private Shader rootShader;
     private Renderer rootRender;
     private Rigidbody[] rocksRigids;
@@ -17,27 +19,52 @@ public class GlitchRoots : MonoBehaviour {
     void Start()
     {
         rigidDebris = debris.GetComponent<Rigidbody>();
-        rocksRigids = new Rigidbody[4];
+        rocksRigids = new Rigidbody[debris.transform.childCount];
+        debrisScript = new BreakableRock[debris.transform.childCount];
         for (int i = 0; i < debris.transform.childCount; ++i)
         {
+            debrisScript[i] = debris.transform.GetChild(i).GetComponent<BreakableRock>();
             rocksRigids[i] = debris.transform.GetChild(i).GetComponent<Rigidbody>();
         }
         rootRender = rootAssociated.GetComponent<Renderer>();
         rootShader = rootRender.material.shader;
     }
 
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            player.IncreaseActivableBox();
+        }
+    }
+
     void OnTriggerStay(Collider coll)
     {
-        if (manager.isActivable && InputManager.ActiveDevice.Action4.IsPressed && coll.CompareTag("Player"))
+        if (manager.isActivable && coll.CompareTag("Player"))
         {
-            rootRender.material.shader = glitchShader;
-            StartCoroutine(DeactivateGlitch(2.0f));
-            rigidDebris.isKinematic = false;
-            rocksRigids[0].isKinematic = false;
-            rocksRigids[1].isKinematic = false;
-            rocksRigids[2].isKinematic = false;
-            rocksRigids[3].isKinematic = false;
-            manager.isActivable = false;
+            player.boxUIActivated.SetActive(true);
+            if (InputManager.ActiveDevice.Action4.IsPressed)
+            {
+                rootRender.material.shader = glitchShader;
+                StartCoroutine(DeactivateGlitch(2.0f));
+                rocksRigids[0].isKinematic = false;
+                rocksRigids[1].isKinematic = false;
+                rocksRigids[2].isKinematic = false;
+                rocksRigids[3].isKinematic = false;
+                manager.isActivable = false;
+            }
+        }
+        else
+        {
+            player.boxUIActivated.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            player.DecreaseActivableBox();
         }
     }
 
@@ -56,10 +83,13 @@ public class GlitchRoots : MonoBehaviour {
     public void TurnToNormality()
     {
         rootRender.material.shader = rootShader;
-        rigidDebris.isKinematic = true;
         rocksRigids[0].isKinematic = true;
         rocksRigids[1].isKinematic = true;
         rocksRigids[2].isKinematic = true;
         rocksRigids[3].isKinematic = true;
+        debrisScript[0].Restart();
+        debrisScript[1].Restart();
+        debrisScript[2].Restart();
+        debrisScript[3].Restart();
     }
 }
