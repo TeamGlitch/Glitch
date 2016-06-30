5c975451f2dcc35d9bb7c42a2af87c2916a0e180
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-06-30 11:29:32 +0200

    Leaf return to tree when player die

--- Assets/_Scripts/Scene/FallingTreeLeaf.cs
+++ Assets/_Scripts/Scene/FallingTreeLeaf.cs
@@ -18,12 +18,25 @@ public class FallingTreeLeaf : MonoBehaviour {
 	private Rigidbody rb;
 	private bool colliderTouched;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody> ();
-    }
+    private Vector3 initPos;
+    private Vector3 initRot;
 
-	void Update() {
+    private Shader leafInitShader;
+    private Shader brachInitShader;
+    private Material branchInitMaterial;
+    private Material leafInitMaterial;
+
+    // Use this for initialization
+    void Start () {
+		rb = GetComponent<Rigidbody> ();
+        initPos = transform.position;
+        initRot = transform.eulerAngles;
+        leafInitShader = Shader.Find("Legacy Shaders/Transparent/Diffuse");
+        brachInitShader = Shader.Find("Legacy Shaders/Transparent/Diffuse");
+        leafInitMaterial = leafRenderer.material;
+        branchInitMaterial = branchRenderer.material;
+    }
+    void Update() {
         if (colliderTouched)
         {
             timeSinceColliderTouched += Time.deltaTime;
@@ -34,10 +47,10 @@ public class FallingTreeLeaf : MonoBehaviour {
                 branchRenderer.material.shader = transparentShader;
                 leafRenderer.material.shader = transparentShader;
                 timeAfterFall += Time.deltaTime;
+                rb.isKinematic = false;
                 float alpha = Mathf.Lerp(1.0f, 0.0f, timeAfterFall / timeBeforeDisappear);
                 branchRenderer.material.color = new Vector4(branchRenderer.material.color.r, branchRenderer.material.color.g, branchRenderer.material.color.b, alpha);
                 leafRenderer.material.color = new Vector4(leafRenderer.material.color.r, leafRenderer.material.color.g, leafRenderer.material.color.b, alpha);
-                rb.isKinematic = false;
                 if (timeAfterFall > timeBeforeDisappear)
                 {
                     gameObject.SetActive(false);
@@ -46,13 +59,31 @@ public class FallingTreeLeaf : MonoBehaviour {
 		}
 	}
 	
-	void OnTriggerEnter(Collider collider) {
+    public void Reset()
+    {
+        transform.position = initPos;
+        transform.eulerAngles = initRot;
+        leafRenderer.material = leafInitMaterial;
+        branchRenderer.material = branchInitMaterial;
+        leafRenderer.material.shader = leafInitShader;
+        branchRenderer.material.shader = brachInitShader;
+        leafRenderer.material.color = new Vector4(1f, 1f, 1f, 1f);
+        branchRenderer.material.color = new Vector4(1f, 1f, 1f, 1f);
+        colliderTouched = false;
+        timeSinceColliderTouched = 0.0f;
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    void OnTriggerEnter(Collider collider) {
 		if (collider.CompareTag("Player")) {
             SoundManager.instance.PlaySingle(glitch);
 			colliderTouched = true;
+            timeSinceColliderTouched = 0.0f;
             branchRenderer.material.shader = distorsionShader;
             leafRenderer.material.shader = distorsionShader;
-		}
-	}
+        }
+    }
 
 }
