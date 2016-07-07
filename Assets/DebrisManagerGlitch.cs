dd9da6f6e87f2ed490d9b9c20c455835f31243bc
IzabranTebras <krystyan.fs@gmail.com>
2016-07-07 16:05:37 +0200

    Animation of shake added to camera

--- Assets/DebrisManagerGlitch.cs
+++ Assets/DebrisManagerGlitch.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class DebrisManagerGlitch : MonoBehaviour {
 
     public Debris[] debris;
+    public CameraShake shake;
 
     private int rand1;
     private int rand2;
@@ -20,6 +21,7 @@ public class DebrisManagerGlitch : MonoBehaviour {
         rand3 = Random.Range(0, debris.Length);
         debris[rand3].Fall();
 
+        shake.shakeIt = true;
         Invoke("Restart", 5.0f);
     }
 
