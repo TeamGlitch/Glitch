3292665cf9cf153b31f50ea60c63b8fce7ebab11
IzabranTebras <krystyan.fs@gmail.com>
2016-07-20 01:02:45 +0200

    more fixes

--- Assets/_Scripts/Scene/DialoguePoint.cs
+++ Assets/_Scripts/Scene/DialoguePoint.cs
@@ -3,50 +3,19 @@ using System.Collections;
 
 using System.Collections.Generic;
 
-public class DialoguePoint : MonoBehaviour {
+public class DialoguePoint : MonoBehaviour
+{
 
-    public BossArcherIA boss;
-    public BossStageCamera camera;
-	public DialogueScript dialogueScript;
-    public PlayerController player;
-    public Door door;
-	public int sceneNum;
+    public DialogueScript dialogueScript;
+    public int sceneNum;
+    private bool used = false;
 
-	private bool used = false;
-    private bool readyForBattle = false;
-
-	void OnTriggerEnter(Collider coll)
+    void OnTriggerEnter(Collider coll)
     {
-		if((coll.gameObject.CompareTag("Player")) && !used)
-		{
-			dialogueScript.callScene(sceneNum);
-			used = true;
-		}
-	}
-
-    void Update()
-    {
-        if (used)
+        if ((coll.gameObject.CompareTag("Player")) && !used)
         {
-            if (player.allowMovement)
-            {
-                readyForBattle = true;
-                used = false;
-            }
+            dialogueScript.callScene(sceneNum);
+            used = true;
         }
-
-        if (readyForBattle)
-        {
-            door.enabled = true;
-            boss.start = true;
-            Invoke("ZoomCamera", 1.0f);
-            readyForBattle = false;
-        }
-    }
-
-    void ZoomCamera()
-    {
-        camera.ZoomIn();
-        gameObject.SetActive(false);
     }
 }
