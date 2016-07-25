42db6d92a10a5f9de96a6f20027bf23eda4bbc3b
MetroGoldyMayer <amphiros@gmail.com>
2016-07-25 16:49:44 +0200

    Go arrow implemented

---
+++ Assets/_Scripts/Scene/ArrowPointScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowPointScript : MonoBehaviour {
+
+    public GoArrowsScript arrowScript;
+    public Vector3 direction_1;
+    public Vector3 direction_2;
+
+    private bool active = true;
+    void OnTriggerEnter(Collider coll)
+    {
+
+        //If there's a collision with the player
+        if ((coll.gameObject.CompareTag("Player")) && active)
+        {
+            arrowScript.activate(direction_1, direction_2);
+            active = false;
+        }
+
+    }
+}
