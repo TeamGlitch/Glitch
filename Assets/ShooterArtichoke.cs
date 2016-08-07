d133d327534dcf655b40306336bdf332343222ac
IzabranTebras <cristian.fernandezsua@gmail.com>
2016-08-07 02:43:59 +0200

    Added behaviour in shooter artichokes and in roots

---
+++ Assets/ShooterArtichoke.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShooterArtichoke : MonoBehaviour {
+
+    public Seed seed;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+	public void Shoot()
+    {
+        seed.enabled = true;
+        anim.SetBool("Shoot", false);
+    }
+
+    public void RestartShoot()
+    {
+        anim.SetBool("Shoot", true);
+    }
+}
