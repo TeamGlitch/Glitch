bfa68892c41fab0ace1f12b5b1fd3e502447c92c
MetroGoldyMayer <amphiros@gmail.com>
2016-08-11 22:00:33 +0200

    Added level-end message

--- Assets/_Scripts/Scene/EndPointScript.cs
+++ Assets/_Scripts/Scene/EndPointScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Xml;
 
 //Everything is temporal!
 public class EndPointScript : MonoBehaviour {
@@ -8,16 +9,24 @@ public class EndPointScript : MonoBehaviour {
     public Text title;
     public Text subtitle;
 
+    public TextAsset XMLAsset;
+
 	private float endGame = -1;
 
     // Script is disabled until OnTriggerEnter detects the player
 	void OnTriggerEnter(Collider coll){
 
 		if(coll.gameObject.CompareTag("Player")){
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(XMLAsset.text);
+
             titlesGameObject.SetActive(true);
-            title.text = "Level 1... complete?";
+            XmlNode texts = xmlDoc.SelectSingleNode("/Dialogue/Set[@lang = \"" + Configuration.getLanguage() + "\"]/EndMessage/Title");
+            title.text = texts.InnerText;
 			title.color = Color.red;
-			subtitle.text = "Time for a boss";
+            texts = xmlDoc.SelectSingleNode("/Dialogue/Set[@lang = \"" + Configuration.getLanguage() + "\"]/EndMessage/Subtitle");
+            subtitle.text = texts.InnerText;
 			coll.transform.gameObject.GetComponent<PlayerController>().allowMovement = false;
 			endGame = Time.time + 3.0f;
             enabled = true;
