c3790c1fbb72da5a39788bf9325ee9b598158e7c
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-09-04 22:53:43 +0200

    Level 1 improvement

---
+++ Assets/FallingBridgeParts.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingBridgeParts : MonoBehaviour {
+
+    public Rigidbody[] rigidbodyBridgeParts;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        for(int i=0; i < rigidbodyBridgeParts.Length; ++i)
+        {
+            rigidbodyBridgeParts[i].isKinematic = false;
+        }
+    }
+
+}
