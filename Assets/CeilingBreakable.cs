7338a8e980aa9bca2dcc17cc7e9c28f9117c59c0
IzabranTebras <krystyan.fs@gmail.com>
2016-09-07 01:57:19 +0200

    Added functionality to breakable ceiling

---
+++ Assets/CeilingBreakable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CeilingBreakable : MonoBehaviour {
+
+    private Animator anim;
+    private BoxCollider box;
+
+	void Start () 
+    {
+        anim = GetComponent<Animator>();
+        box = GetComponent<BoxCollider>();
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("Death"))
+        {
+            box.enabled = false;
+            anim.SetBool("Break", true);
+        }
+    }
+	
+}
