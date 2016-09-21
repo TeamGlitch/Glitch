d624e5a894540f3549723a0430b4a95f306e3a7d
MetroGoldyMayer <amphiros@gmail.com>
2016-09-22 00:40:23 +0200

    Clouds and fixed wall-to-wall

---
+++ Assets/TutorialClouds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialClouds : MonoBehaviour {
+
+    //public RawImage backLayer;
+    public RawImage frontLayer;
+	
+	// Update is called once per frame
+	void Update () {
+        Rect rect = frontLayer.uvRect;
+        rect.x += 0.2f * Time.deltaTime;
+        frontLayer.uvRect = rect;
+	}
+}
