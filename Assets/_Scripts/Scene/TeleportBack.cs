299681343551eb2d79efc9d591f81eb103369a7b
MetroGoldyMayer <amphiros@gmail.com>
2016-09-24 00:40:07 +0200

    Teleport if you fall to the left

---
+++ Assets/_Scripts/Scene/TeleportBack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportBack : MonoBehaviour {
+
+    public Player player;
+    public StartPoint startpoint;
+    public AudioClip audio;
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            Vector3 newPosition = startpoint.transform.position;
+            newPosition.z = player.transform.position.z;
+            player.transform.position = newPosition;
+            SoundManager.instance.PlaySingle(audio);
+        }
+        
+
+    }
+
+}
