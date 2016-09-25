31137230727fc30e1da4d5b3044154e806dec015
Oriol Romero Herrando <oriol.romero.herrando@gmail.com>
2016-09-25 17:56:53 +0200

    scene improvements

---
+++ Assets/ChangingGlobalFog.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangingGlobalFog : MonoBehaviour {
+
+    public UnityStandardAssets.ImageEffects.GlobalFog globalFog;
+
+    private float timeToChangeFog = 1f;
+    public float endDistanceFog;
+    private float startingDistanceFog;
+    private float timeChanging;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            timeChanging = 0f;
+            startingDistanceFog = globalFog.startDistance;
+        }
+    }
+
+    IEnumerator ChangeFog()
+    {
+        while (timeChanging < timeToChangeFog)
+        {
+            timeChanging += Time.deltaTime;
+            if (timeChanging >= timeToChangeFog)
+                timeChanging = timeToChangeFog;
+            globalFog.startDistance = Mathf.Lerp(startingDistanceFog, endDistanceFog, timeChanging / timeToChangeFog);
+            yield return null;
+        }
+    }
+
+
+}
--- Assets/_Scripts/Scene/Pirana.cs
+++ Assets/_Scripts/Scene/Pirana.cs
@@ -1,76 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pirana : MonoBehaviour {
+public class Pirana : MonoBehaviour
+{
 
-	public World world;
+    public World world;
 
-	private bool jumping;
+    private bool jumping;
 
-	public float jumpAltitude = 10f;
-	public float jumpTime = 2.5f;
-	public float waitingTimeToAddForce = 10.0f;
-	public float possibleVariation = 1.0f;
+    public float jumpAltitude = 10f;
+    public float jumpTime = 2.5f;
+    public float waitingTimeToAddForce = 10.0f;
+    public float possibleVariation = 1.0f;
+    public float changingZ = 0.4f;
 
-	private float initialY;
+    private float initialZ;
+    private float initialY;
 
-	private float jumpStart = 0f;
-	private float lastUpdateTime = 0f;
-	private float nextJump;
+    private float jumpStart = 0f;
+    private float lastUpdateTime = 0f;
+    private float nextJump;
 
 
-	// Use this for initialization
-	void Start () {
-		initialY = transform.position.y;
-		jumpStart = Time.time;
-		lastUpdateTime = Time.time;
-		jumping = true;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (world.doUpdate){
+    // Use this for initialization
+    void Start()
+    {
+        initialZ = transform.position.z;
+        initialY = transform.position.y;
+        jumpStart = Time.time;
+        lastUpdateTime = Time.time;
+        jumping = true;
+    }
 
-			//If we're jumping
-			if (jumping) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (world.doUpdate)
+        {
 
-				//We calculate where in the animation we are
-				//The actual time is lasUpdateTime + world.lag, witch is .deltaTime (elapsed time
-				//since last update) in normal conditions and the time passed with slowdown when
-				//slowFPS are activated
-				float timePassed = ((lastUpdateTime + world.lag) - jumpStart) / jumpTime;
-				lastUpdateTime = Time.time;
+            //If we're jumping
+            if (jumping)
+            {
 
-				//If the animation is complete, deactivate the jumping state
-				//and prepare the next jump
-				if (timePassed >= 1) {
-					timePassed = 0;
-					jumping = false;
-					nextJump = Time.time + Random.Range (waitingTimeToAddForce - possibleVariation, waitingTimeToAddForce + possibleVariation);
-				}
+                //We calculate where in the animation we are
+                //The actual time is lasUpdateTime + world.lag, witch is .deltaTime (elapsed time
+                //since last update) in normal conditions and the time passed with slowdown when
+                //slowFPS are activated
+                float timePassed = ((lastUpdateTime + world.lag) - jumpStart) / jumpTime;
+                lastUpdateTime = Time.time;
 
-				//Does the function
-				//T * sin^2(((x-x^2)*T)/PI)*2
-				//T = Highest altitude
-				//x = Time elapsed (0->1)
-				//that does a up-down movement with smoothing at the top
-				Vector3 newPos = transform.position;
+                //If the animation is complete, deactivate the jumping state
+                //and prepare the next jump
+                if (timePassed >= 1)
+                {
+                    timePassed = 0;
+                    jumping = false;
+                    nextJump = Time.time + Random.Range(waitingTimeToAddForce - possibleVariation, waitingTimeToAddForce + possibleVariation);
+                }
 
-				float calc = jumpAltitude * (timePassed - 1) * timePassed;
-				calc /= Mathf.PI;
-				newPos.y = initialY + (2 * jumpAltitude * Mathf.Pow (Mathf.Sin (calc), 2));
+                //Does the function
+                //T * sin^2(((x-x^2)*T)/PI)*2
+                //T = Highest altitude
+                //x = Time elapsed (0->1)
+                //that does a up-down movement with smoothing at the top
+                Vector3 newPos = transform.position;
+                Vector3 newRot = new Vector3(270f, 0, 90f);
 
-				transform.position = newPos;
+                float calc = jumpAltitude * (timePassed - 1) * timePassed;
+                calc /= Mathf.PI;
+                newPos.y = initialY + (2 * jumpAltitude * Mathf.Pow(Mathf.Sin(calc), 2));
+                newPos.z = Mathf.Lerp(initialZ, initialZ + changingZ, timePassed);
 
-			}
-			//If we're not jumping but the time to jump has come
-			else if (Time.time > nextJump) {
-				jumpStart = Time.time;
-				lastUpdateTime = Time.time;
-				jumping = true;
-			}
-		}
-	}
+                newRot.x = Mathf.Lerp(270f, 90f, timePassed);
+
+                transform.position = newPos;
+
+                //    Debug.Log(transform.eulerAngles);
+
+                transform.eulerAngles = newRot;
+
+            }
+            //If we're not jumping but the time to jump has come
+            else if (Time.time > nextJump)
+            {
+                jumpStart = Time.time;
+                lastUpdateTime = Time.time;
+                jumping = true;
+            }
+        }
+    }
 
 }
