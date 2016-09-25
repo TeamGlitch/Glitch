a53d59cb4d430671b67afcd08619cd6377d08dc2
MetroGoldyMayer <amphiros@gmail.com>
2016-09-26 00:34:12 +0200

    Items, special items, inmortality and time

---
+++ Assets/_Scripts/Scene/TutorialWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialWorld : MonoBehaviour {
+
+    public Player player;
+
+	// Use this for initialization
+	void Start () {
+        player.godmode = true;
+	}
+	
+	// Update is called once per frame
+	/*
+    void Update () {
+	
+	}
+    */
+}
