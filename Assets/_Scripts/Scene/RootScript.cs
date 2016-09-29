4a1831d4339a5e2b63cddd8339b4ab60e976a71f
IzabranTebras <krystyan.fs@gmail.com>
2016-09-29 13:33:17 +0200

    Added script to roots

--- Assets/_Scripts/Scene/RootScript.cs
+++ Assets/_Scripts/Scene/RootScript.cs
@@ -3,161 +3,32 @@ using System.Collections;
 
 public class RootScript : MonoBehaviour {
 
-    public World world;
-    public bool horizontal = false;
-    public bool down = true;
-    public bool toLeft = false;
+    public GameObject root;
+    public float waitingTime;
 
     private Animator anim;
-    private bool top;
-    private bool right;
     private bool stop = false;
 
     void Start()
     {
-        anim = GetComponent<Animator>();
-        if (horizontal)
-        {
-            if (toLeft)
-            {
-                right = true;
-            }
-            else
-            {
-                right = false;
-            }
-
-        }
-        else
-        {
-            if (down)
-            {
-                top = true;
-            }
-            else
-            {
-                top = false;
-            }
-        }
+        anim = root.GetComponent<Animator>();
     }
 
     void OnTriggerEnter(Collider coll)
     {
-        if (!coll.CompareTag("Player"))
+        if (!stop && coll.CompareTag("Player"))
         {
-            if (horizontal)
-            {
-                if (!right)
-                {
-                    right = true;
-                    stop = true;
-                    // Is in origin
-                    if (toLeft)
-                    {
-                        Invoke("Reactivate", 4.0f);
-                    }
-                    else
-                    {
-                        Invoke("Reactivate", 2.0f);
-                    }
-                }
-                else
-                {
-                    right = false;
-                    stop = true;
-                    // Is in origin
-                    if (!toLeft)
-                    {
-                        Invoke("Reactivate", 4.0f);
-                    }
-                    else
-                    {
-                        Invoke("Reactivate", 2.0f);
-                    }
-                }
-            }
-            else
-            {
-                if (!down)
-                {
-                    down = true;
-                    if (top)
-                    {
-                        enabled = false;
-                    }
-                    else
-                    {
-                        stop = true;
-                        Invoke("Reactivate", 2.0f);
-                    }
-                }
-                else
-                {
-                    down = false;
-                    if (!top)
-                    {
-                        enabled = false;
-                    }
-                    else
-                    {
-                        stop = true;
-                        Invoke("Reactivate", 2.0f);
-                    }
-                }
-            }
+            anim.SetBool("Extend", true);
+            stop = true;
+            Invoke("Shrink", waitingTime);
         }
     }
 
-	void Update () {
-        if (world.doUpdate)
-        {
-            if (!stop)
-            {
-                if (horizontal)
-                {
-                    if (toLeft)
-                    {
-                        if (!right)
-                        {
-                            anim.SetBool("Extend", false);
-                            //transform.Translate(world.lag * 10, 0.0f, 0.0f);
-                        }
-                        else
-                        {
-                            anim.SetBool("Extend", true);
-                           //transform.Translate(-world.lag * 50, 0.0f, 0.0f);
-                        }
-                    }
-                    else
-                    {
-                        if (!right)
-                        {
-                            anim.SetBool("Extend", true);
-                            //transform.Translate(world.lag * 50, 0.0f, 0.0f);
-                        }
-                        else
-                        {
-                            anim.SetBool("Extend", false);
-                            //transform.Translate(-world.lag * 10, 0.0f, 0.0f);
-                        }
-                    }
-                }
-                else
-                {
-                    if (down)
-                    {
-                        anim.SetBool("Extend", true);
-                        //transform.Translate(0.0f, -world.lag * 30, 0.0f);
-                    }
-                    else
-                    {
-                        anim.SetBool("Extend", false);
-                        //transform.Translate(0.0f, world.lag * 10, 0.0f);
-                    }
-                }
-            }
-        }
-	}
+    public void Shrink()
+    {
+        anim.SetBool("Extend", false);
+        Invoke("Reactivate", 1.0f);
+    }
 
     public void Reactivate()
     {
