adfec57388f99f6de5a292852f3b9feebd4dae9c
IzabranTebras <krystyan.fs@gmail.com>
2016-10-03 19:43:10 +0200

    Added last features

---
+++ Assets/Elevator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+    public World world;
+    public float speed;
+    public bool up;
+	
+	void Update () 
+    {
+        if (world.doUpdate)
+        {
+            if (up)
+            {
+                transform.Translate(0.0f, speed * world.lag, 0.0f);
+            }
+            else
+            {
+                transform.Translate(0.0f, -(speed * world.lag), 0.0f);
+            }
+        }
+	}
+}
---
+++ Assets/StartElevator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartElevator : MonoBehaviour {
+
+    public Elevator elevator;
+    public GameObject sideCollider1;
+    public GameObject sideCollider2;
+    public Transform player;
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            player.parent = elevator.transform;
+            elevator.enabled = true;
+            sideCollider1.SetActive(true);
+            sideCollider2.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
